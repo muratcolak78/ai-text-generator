26c78c739a0046dc8c2e23b4cc41a5ec42916668
Muhammad Yahya <yahya@wightcode.co.uk>
2025-05-14 11:24:02 +0100

    initial commit

---
+++ src/main/java/com/example/supporttemplate/SupportTemplateGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.supporttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTemplateGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportTemplateGeneratorApplication.class, args);
+	}
+
+}
